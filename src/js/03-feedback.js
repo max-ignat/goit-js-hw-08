cdf0ae587d39394d2b9076e931655df666a3e460
max-ignat <cadillac.maxim@gmail.com>
2022-11-17 10:26:49 +0200

    summary

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,40 @@
 import throttle from 'lodash.throttle';
-const formEl = document.querySelector('form');
-const inputEl = document.querySelector('input');
-const textEl = document.querySelector('textarea');
+
+const form = document.querySelector('form');
+
+const input = document.querySelector('input');
+
+const text = document.querySelector('textarea');
+
 const STORAGE_KEY = 'feedback-form-state';
 
+form.addEventListener('input', throttle(saveDataToStorage, 500));
+form.addEventListener('submit', onFormSubmit);
 
+getData();
 
+function saveDataToStorage() {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
 
-// const onInputChange = (event) => {
-//     localStorage.setItem('feedback-form-state', event.elements.currentTarget.value);
-// };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
+}
 
-// const formData = {
-//     mail,
-//     message,
-//     };
-//     console.log(formData);
-//     form.reset();
+function onFormSubmit(event) {
 
-form.addEventListener('input', throttle(setDataToStorage, 500));
-form.addEventListener('submit', onSubmit);
+    if (form.value === '' || text.value === ''){
+        return alert(' feel up all form!');}
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
 
+function getData(event) {
+  const dataFromStorage = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(dataFromStorage);
+
+  if (dataFromStorage) {
+    input.value = parsedData.email || '';
+    text.value = parsedData.message || '';
+  }
+}
